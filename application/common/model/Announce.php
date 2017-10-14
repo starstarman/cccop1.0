e5afc0d23bf4ff4d2363e0acaad72f2e130c1e53
baijiang12 <2296772442@qq.com>
2017-10-14 09:40:11 +0800

    cccop1.0.9 公告查看的功能，模型新增了方法

---
+++ application/common/model/Announce.php
@@ -0,0 +1,32 @@
+<?php
+namespace app\common\model;
+use think\Model;
+
+class Announce extends Model
+{
+     public function getAnnounceElemenById(){
+         $order = [
+             'id' => 'asc',//降序desc
+         ];
+
+         $data = [
+         ];
+         $result = $this->where($data)
+             ->order($order)
+             ->paginate(1);
+         return $result;
+     }
+    public function getAnnounceElemenById1($id){
+        $order = [
+            'id' => 'asc',//降序desc
+        ];
+
+        $data = [
+            'id'=>$id,
+        ];
+        $result = $this->where($data)
+            ->order($order)
+            ->select();
+        return $result;
+    }
+}
