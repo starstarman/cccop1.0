349f8aa1351a50167985f4c3dfc7aa1ad1ba91d6
starstarman <83170989@qq.com>
2017-10-18 20:23:26 +0800

    cccop1.2.0 添加了 学生提交功能

--- application/index/controller/Form.php
+++ application/index/controller/Form.php
@@ -34,6 +34,41 @@ class Form extends Controller
         ];
         model('Adminform')->save($content);
     }
+/*
+ * 学生提交表单
+ */
+    public function studentformSubmit(){
+        //获取学生的id
+        $userinfo=session('userinfo','','index');
+        $s_id=$userinfo[0]['id'];
+        //获取提交表单的内容
+        $data=input('param.');
+        $content=[
+            'f_id'=>$data['f_id'],
+            's_id'=>$s_id,
+            'formName'=>$data['formName'],
+            'html'=>$data['html'],
+            'user_1'=>$data['user_1'],
+            'user_2'=>$data['user_2'],
+            'user_3'=>$data['user_3'],
+            'user_4'=>$data['user_4'],
+            'user_5'=>$data['user_5'],
+            'user_6'=>$data['user_6'],
+            'user_7'=>$data['user_7'],
+        ];
+        //把内容存到总表Form中
+        $id=[
+            'f_id'=>$data['f_id'],
+            's_id'=>$s_id,
+        ];
+        //如果总表里有数据 更新总表的  总表里没有数据就添加到总表
+        if (model('Form')->where($id)->select()){
+            model('Form')->isUpdate(true)->save($content);
+        }else{
+            model('Form')->save($content);
+        }
+
+    }
 
     /**
      *  实习审批表查看表单   查询管理员建的表单
@@ -64,12 +99,24 @@ class Form extends Controller
             ];
             $result=model('Form')->where($data)->select();
             if (!empty($result)){
-                echo '这里有数据';
+                return $this->fetch('',[
+                    'f_id'=>$f_id,
+                    'html'=>$result[0]['html'],
+                    'identity'=>$identity,
+                    'user_1'=>$result[0]['user_1'],
+                    'user_2'=>$result[0]['user_2'],
+                    'user_3'=>$result[0]['user_3'],
+                    'user_4'=>$result[0]['user_4'],
+                    'user_5'=>$result[0]['user_5'],
+                    'user_6'=>$result[0]['user_6'],
+                    'user_7'=>$result[0]['user_7'],
+                ]);
             }else{
                 $data=model('Adminform')->where('id',$f_id)->select();
             }
-
+        //返给学生提交用的必用f_id
         return $this->fetch('',[
+                'f_id'=>$f_id,
                 'html'=>$data[0]['html'],
                 'identity'=>$identity,
                 'user_1'=>$data[0]['user_1'],
--- runtime/temp/dc864a31f10aacf05c9bfd95aa16a48e.php
+++ runtime/temp/dc864a31f10aacf05c9bfd95aa16a48e.php
@@ -1,4 +1,4 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:81:"F:\Apache24\htdocs\cccop1.0\public/../application/index\view\form\createform.html";i:1508241857;}*/ ?>
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:81:"F:\Apache24\htdocs\cccop1.0\public/../application/index\view\form\createform.html";i:1508247673;}*/ ?>
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -133,7 +133,7 @@
                                     <label class="control-label leipiplugins-orgname">文本框</label>
                                     <div class="controls">
                                         <input name="leipiNewField" type="text" placeholder="默认值" title="文本框" value=""
-                                               class="leipiplugins" leipiplugins="text" shenfen=""/>
+                                               class="leipiplugins" leipiplugins="text" shenfen="">
                                     </div>
 
                                 </div>
