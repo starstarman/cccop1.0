f5c60d4a9b5997286fb27b6fdc55adea975adfad
Yuejiawei <y2219531345>
2017-10-30 17:02:45 +0800

    cccop1.2.1登录验证、弹窗实现，session的获取和使用

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -12,20 +12,34 @@ class Login extends Base
     {
         if (request()->isPost()) {
             $data = input('post.');
-
+          /* $ret =  model('user')->gidet([''=>$data['id']]);
+           if(!$ret)
+           {
+               $this->error('该用户不存在');
+           }
+           if($ret->password != $data['password'])
+           {
+               $this->error('密码不正确');
+           }
+           session('login',$ret,'index');
+           return $this->success('登陆成功','login/loginSuccess');*/
             //进行验证码验证
             //if (!captcha_check($data['verifycode'])) {
               //  $this->error('验证码不正确');
             //} else {
-                $username = $data['username'];
+                //$username = $data['username'];
+                $id = $data['id'];
+                //$identity = $data['identity'];
                 //根据姓名获取登陆者的信息
-
-                $result = Db::query("select * from cop_user WHERE username = '$username'");
-                $_SESSION['username'] = $data['username'];
+                $result = Db::query("select * from cop_user WHERE id = '$id'");
+                //$_SESSION['identity'] = $data['identity'];
+                //$_SESSION['username'] = $data['username'];
                 if ($result) {
                     foreach ($result as $ret) {
+                        $_SESSION['username']=$ret['username'];
+                        $_SESSION['identity']=$ret['identity'];
                     }
-                    session('userinfo', $result, 'index');
+                    //session('userinfo', $result, 'index');
                     //进行密码验证
                     if ($ret['password'] == $data['password']) {
                         $this->success('登陆成功！','login/loginSuccess',1,1);
@@ -48,6 +62,13 @@ class Login extends Base
     public function welcome(){
         //return $this->fetch();
         return '欢迎来到校企合作';
+    }
+    public function logout()
+    {
+        //清空session
+        session('null','index');
+        //跳转
+        $this->redirect('login/login');
     }
 
 }
