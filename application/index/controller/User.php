f78428979a13c85e259b13f0a4b567f1662d1d63
baijiang12 <2296772442@qq.com>
2017-12-17 10:49:00 +0800

    用户模块的控制器

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -45,24 +45,7 @@ class User extends Controller
             'select_num2'=>$num
         ]);
     }
-    public function member_teacher_list1(){
-        //获取显示条数
-//        $data = input('param.');
-//        return $data['select_num'];
-        $data=input('param.');
-        print_r($data['num']);
-        die();
-        //查看教师
-        $user_teacher = $this->obj->getUserByIdentify_t($data['select_num']);
-        //计算教师总数
-        $user_num = $this->obj->userCount(0,1);
-        return $this->fetch('member_teacher_list', [
-            'user'=>$user_teacher,
-            'user_num'=>$user_num
-        ]);
-    }
-
-    //根据用户信息进行搜索
+   //根据用户信息进行搜索
     public function search(){
         $data = input('get.');
         $search_user = $this->obj->getUserByName($data['name']);
@@ -107,19 +90,33 @@ class User extends Controller
     }
 
     public function member_student_del(){
-        $user_student = $this->obj->getDelUser(1);
+        $data=input('param.');
+        if (!empty($data['select_num'])){
+            $num=$data['select_num'];
+        }else{
+            $num = 5;
+        }
+        $user_student = $this->obj->getDelUser($num);
         $user_num = $this->obj->userCount(1,0);
         return $this->fetch('', [
             'user'=>$user_student,
-            'user_num'=>$user_num
+            'user_num'=>$user_num,
+            'select_num2'=>$num
         ]);
     }
     public function member_teacher_del(){
-        $user_teacher = $this->obj->getDelUser_t();
+        $data=input('param.');
+        if (!empty($data['select_num'])){
+            $num=$data['select_num'];
+        }else{
+            $num = 5;
+        }
+        $user_teacher = $this->obj->getDelUser_t($num);
         $user_num = $this->obj->userCount(0,0);
         return $this->fetch('', [
             'user'=>$user_teacher,
-            'user_num'=>$user_num
+            'user_num'=>$user_num,
+            'select_num2'=>$num
         ]);
     }
 
