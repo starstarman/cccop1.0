87f140ed5a2cd0dce02501d16ba01118288a4491
starstarman <83170989@qq.com>
2017-12-18 17:45:45 +0800

    cccop 1.3.9多重身份完成

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -36,17 +36,20 @@ class Login extends Base
                 $result = Db::query("select * from cop_user WHERE id = '$id'");
                 //$_SESSION['identity'] = $data['identity'];
                 //$_SESSION['username'] = $data['username'];
+
                 if ($result) {
-                    foreach ($result as $ret) {
-                        session('login',$ret,'index');
-                        $_SESSION['username']=$ret['username'];
-                        $_SESSION['identity']=$ret['identity'];
-                        session('id',$ret['id']);
-                    }
-                    //session('userinfo', $result, 'index');
+                        $_SESSION['username']=$result[0]['username'];
+                        $_SESSION['identity']=$result[0]['identity'];
+                        session('id',$result[0]['id']);
+                        session('username',$result[0]['username']);
+                        session('identity',$result[0]['identity']);
+
+                        foreach ($result as $key){
+                                $identity[]=$key['identity'];
+                            }
                     //进行密码验证
-                    if ($ret['password'] == $data['password']) {
-                        $this->success('','login/loginSuccess');
+                    if ($result[0]['password'] == $data['password']) {
+                        $this->success('',url('login/loginSuccess',['identity'=>$identity]));
 
                     } else {
                         return $this->error('密码不正确！');
@@ -59,7 +62,38 @@ class Login extends Base
     //}
     //登录成功后进行跳转
     public function loginSuccess(){
-        return $this->fetch();
+        $data=input('param.');
+        for($i=0;$i<count($data['identity']);$i++){
+            //将接收到的数据进行转换
+            switch ($data['identity'][$i]){
+                case '2':
+                    $identity[$i]='班主任';
+                    break;
+                case '3':
+                    $identity[$i]='导员';
+                    break;
+                case '4':
+                    $identity[$i]='书记';
+                    break;
+                case '5':
+                    $identity[$i]='指导教师';
+                    break;
+                case '6':
+                    $identity[$i]='系主任';
+                    break;
+                case '7':
+                    $identity[$i]='院长';
+                    break;
+                case '':
+                    $identity[$i]='';
+                    break;
+            }
+        }
+        return $this->fetch('',[
+            'identity'=>$identity,
+            'change'=>$data['identity'],
+            'many'=>count($data['identity'])
+        ]);
     }
 
     //欢迎界面的
@@ -78,4 +112,11 @@ class Login extends Base
         $this->redirect('login/login');
     }
 
+        public function change(){
+        $data=input('param.');
+        print_r($data);
+        session('identity',$data['identity']);
+        $_SESSION['identity']=$data['identity'];
+        return show('1');
+    }
 }
