bf528f0cbfe40e5641ddf1ec40ef69eda5eb994e
Yuejiawei <y2219531345>
2018-04-05 12:06:51 +0800

    修改部分功能1.6.8

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -8,29 +8,4 @@ class Base extends Controller
     public function index(){
         return $this->fetch();
     }
-    /*public function _initialize()
-    {
-        //判断用户是否登陆
-        $isLogin = $this->isLogin();
-        if(!$isLogin)
-        {
-            return $this->redirect(url('login/login'));
-        }
-    }
-    public function isLogin()
-    {
-        $user=$this->getLoginUser();
-        if($user && $user->id)
-        {
-            return true;
-        }
-        return false;
-    }
-    public function getLoginUser()
-    {
-        if($this->login){
-            $this->login = session('login','','index');
-        }
-        return $this->login;
-    }*/
 }
--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -14,30 +14,12 @@ class Login extends Base
     {
         if (request()->isPost()) {
             $data = input('post.');
-          /* $ret =  model('user')->gidet([''=>$data['id']]);
-           if(!$ret)
-           {
-               $this->error('该用户不存在');
-           }
-           if($ret->password != $data['password'])
-           {
-               $this->error('密码不正确');
-           }
-
-           return $this->success('登陆成功','login/loginSuccess');*/
-            //进行验证码验证
-            //if (!captcha_check($data['verifycode'])) {
-              //  $this->error('验证码不正确');
-            //} else {
-                //$username = $data['username'];
-
+            if (!captcha_check($data['verifycode'])) {
+                $this->error('验证码不正确');}
                 $id = $data['id'];
-                //$identity = $data['identity'];
                 //根据姓名获取登陆者的信息
                 $result = Db::query("select * from cop_user WHERE id = '$id'");
                 session('login',$result,'index');
-                //$_SESSION['identity'] = $data['identity'];
-                //$_SESSION['username'] = $data['username'];
 
                 if ($result) {
                         $_SESSION['username']=$result[0]['username'];
@@ -189,8 +171,6 @@ class Login extends Base
         Session::start();
         //清空session
         Session::destroy();
-        //Session::set('username','null','index');
-        //session('null','index');
         //跳转
         $this->redirect('login/login');
     }
